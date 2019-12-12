5e9d3cdab213261316419f4da7a62143eed78e62
pgalhardo <pedromgalhardo@tecnico.ulisboa.pt>
2019-12-12 17:31:41 +0000

    Fix populate.sql

--- proj3/populate.sql
+++ proj3/populate.sql
@@ -50,26 +50,43 @@ insert into anomalia_traducao values (17, '((10,10), (20,20))', 'ingles');
 -- Duplicado
 insert into duplicado values (2, 3);
 
+ALTER TABLE utilizador_regular
+	  ALTER CONSTRAINT fk_utilizador_regular_email
+	  DEFERRABLE INITIALLY IMMEDIATE;
+  
+ALTER TABLE utilizador_qualificado
+	  ALTER CONSTRAINT fk_utilizador_qualificado_email
+	  DEFERRABLE INITIALLY IMMEDIATE;
+
 -- Utilizador
 START TRANSACTION;
-insert into utilizador values ('gabriel.almeida@tecnico.ulisboa.pt', '12345');
+SET CONSTRAINTS ALL DEFERRED;
 insert into utilizador_regular values ('gabriel.almeida@tecnico.ulisboa.pt');
+insert into utilizador values ('gabriel.almeida@tecnico.ulisboa.pt', '12345');
 COMMIT;
+
 START TRANSACTION;
-insert into utilizador values ('marcelo.rebelo.sousa@gmail.com', 'fdksjhfj');
+SET CONSTRAINTS ALL DEFERRED;
 insert into utilizador_qualificado values ('marcelo.rebelo.sousa@gmail.com');
+insert into utilizador values ('marcelo.rebelo.sousa@gmail.com', 'fdksjhfj');
 COMMIT;
+
 START TRANSACTION;
-insert into utilizador values ('pgalhardo@me.com', '123');
+SET CONSTRAINTS ALL DEFERRED;
 insert into utilizador_qualificado values ('pgalhardo@me.com');
+insert into utilizador values ('pgalhardo@me.com', '123');
 COMMIT;
+
 START TRANSACTION;
-insert into utilizador values ('daniel@me.com', '123');
+SET CONSTRAINTS ALL DEFERRED;
 insert into utilizador_qualificado values ('daniel@me.com');
+insert into utilizador values ('daniel@me.com', '123');
 COMMIT;
+
 START TRANSACTION;
-insert into utilizador values ('admin', 'admin');
+SET CONSTRAINTS ALL DEFERRED;
 insert into utilizador_qualificado values ('admin');
+insert into utilizador values ('admin', 'admin');
 COMMIT;
 
 -- Incidencia
@@ -90,7 +107,6 @@ insert into incidencia values (14, 4, 'daniel@me.com');
 insert into incidencia values (15, 4, 'daniel@me.com');
 insert into incidencia values (16, 4, 'daniel@me.com');
 insert into incidencia values (17, 4, 'daniel@me.com');
-
 
 -- Proposta de Correcao
 insert into proposta_de_correcao values ('marcelo.rebelo.sousa@gmail.com', default, now(), 'Em vez de otorrinoleringlogista deveria ser otorrinolaringologista');
